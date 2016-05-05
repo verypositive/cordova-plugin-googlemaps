2d450ce1a636a1bb19f443ec4c04dd1aa3891b50
Paul Tsupikoff <goodsoft@goodsoft.lv>
2016-05-06 01:26:24 +0300

    Workaround for compatibility with non-overlayed StatusBar

--- src/ios/GoogleMaps/GoogleMaps.m
+++ src/ios/GoogleMaps/GoogleMaps.m
@@ -24,7 +24,9 @@
     //[self versionCheck];
 
 
-    self.pluginLayer = [[MyPluginLayer alloc] initWithFrame:self.webView.frame];
+    CGRect viewBounds = [self.webView bounds];
+    viewBounds.size.height = viewBounds.size.height + 20;
+    self.pluginLayer = [[MyPluginLayer alloc] initWithFrame:viewBounds];
     self.pluginLayer.backgroundColor = [UIColor whiteColor];
     self.pluginLayer.webView = self.webView;
     self.pluginLayer.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
